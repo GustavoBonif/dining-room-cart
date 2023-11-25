47789d67b0027540d1c3e0e25546ef42e92b4146
GustavoBonif <gubonic@gmail.com>
2023-11-25 10:30:03 -0300

    setup: creating cart project

---
+++ src/main/java/com/diningroom/cart/DiningRoomCartApplication.java
@@ -0,0 +1,13 @@
+package com.diningroom.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningRoomCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiningRoomCartApplication.class, args);
+    }
+
+}
